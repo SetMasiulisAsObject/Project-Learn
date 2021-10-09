9568b520168b1881cec5c4acd689e3830554ec80
SetMasiulisAsObject <mindaug.masiulis@gmail.com>
2021-10-09 18:40:21 +0300

    some silly notes on index page

--- pages/index.js
+++ pages/index.js
@@ -13,25 +13,21 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.j          sssssssssss!</a>
-          <a>Works on Netlify</a>
+                    <a>This site works on Netlify server</a>
         </h1>
 
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Laba diena. Hello. Hej. 
             </p>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.facebook.com/mindaugas.masiulis.5"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Contact developer Mindaugas Masiulis on Facebook
         </a>
       </footer>
     </div>
